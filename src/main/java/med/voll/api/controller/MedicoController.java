a30633af58d7c826905ecf794a2f66c66c0086fd
Eduardo Fleury <eduardopinafleury@gmail.com>
2023-07-26 16:26:26 -0300

    Recebendo dados com metodo GET e controlando paginacao

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,14 +3,18 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/medicos")
@@ -21,5 +25,14 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         mr.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable paginacao){
+        Page<Medico> medicos = mr.findAll(paginacao);
+        Page<DadosListagemMedico> medicoList = medicos.map(medico -> new DadosListagemMedico(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade()));
+
+        //return mr.findAll().stream().map(DadosListagemMedico::new).toList(); TEM ESSA OPÇÃO TBM
+        return medicoList;
     }
 }
